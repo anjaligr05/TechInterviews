3b06825046bf137dc62e9278d7662a3f6409f63b
Anjali Reddy <anjred@amazon.com>
2018-06-10 22:17:38 -0700

    Merge two arrays in place

--- grokking_algorithms/mergeInPlace.py
+++ grokking_algorithms/mergeInPlace.py
@@ -21,4 +21,17 @@ def mergeEle(arr, temp):
 		arr.append(temp)
 	return arr
 mergeInPlace([4,5, 7,8, 11],[2,3, 6,6, 10])
-
+def merger(arr1, arr2, m, n):
+	for i in range(n-1, -1, -1):
+		last = arr1[m-1]
+		j=m-2
+		while(j>=0 and arr1[j]>arr2[i]):
+			arr1[j+1]=arr1[j]
+			j = j-1
+		if j!=m-2 or last>arr2[i]:
+			arr1[j+1] = arr2[i]
+			arr2[i]=last
+	print arr1
+	print arr2
+mergeInPlace([4,5, 7,8, 11],[2,3, 6,6, 10])
+merger([4,5,7,8,11],[2,3,6,6,10],5,5)
